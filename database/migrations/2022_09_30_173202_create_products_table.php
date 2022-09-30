38b2a6be928bea685a0a0266ac22612acc856f65
AdrianoMiguell <adrianosantos260804@gmail.com>
2022-09-30 14:40:42 -0300

    teste3

--- database/migrations/2022_09_30_173202_create_products_table.php
+++ database/migrations/2022_09_30_173202_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
